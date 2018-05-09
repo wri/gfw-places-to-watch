3276ca68afbfffaea71d90d1819a93a008085aac
mappingvermont <charlie.hofmann@gmail.com>
2018-05-09 15:38:41 -0400

    fix help params, pass hadoop_pip input as list

--- update-ptw.py
+++ update-ptw.py
@@ -20,15 +20,15 @@ def main():
 
     parser = argparse.ArgumentParser(description='Run hadoop process to count GLAD by PTW grid cell')
     parser.add_argument('--min-date', required=True, type=val.valid_date,
-                        help='the minimum alert date (%Y-%m-%d) to consider for this run (inclusive)')
+                        help='the minimum alert date (YYYY-mm-dd) to consider for this run (inclusive)')
     parser.add_argument('--max-date', required=True, type=val.valid_date,
-                        help='the maximum alert date (%Y-%m-%d) to consider for this run (inclusive)')
+                        help='the maximum alert date (YYYY-mm-dd) to consider for this run (inclusive)')
     parser.add_argument('--staging', dest='staging', action='store_true', help='pushes results to staging table')
     args = parser.parse_args()
 
     # start hadoop cluster, run analysis with hadoop_ptw.ini as input
     ptw_config = os.path.join('processing', 'hadoop_ptw.ini')
-    s3_result_output = run_pip.run(ptw_config)
+    s3_result_output = run_pip.run([ptw_config])
 
     # grab results from s3, tabulate glad count * importance, pick top 10 for each region
     top_10_results = calculate_grid_score.tabulate_results(s3_result_output, **vars(args))
