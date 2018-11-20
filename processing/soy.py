9f6cbc260151e99d2793237cec0aa6dba6f28711
Ifeoma Collins <ifeomacollins@users.noreply.github.com>
2018-11-20 12:27:53 -0500

    test commit

--- processing/soy.py
+++ processing/soy.py
@@ -3,6 +3,7 @@
 def tabulate_and_pick_top10(df):
 
     print 'doing some soy process!'
+    print 'more soy joy'
 
     # must return our output in this 'records' format
     # so that we can upload it to CARTO properly
